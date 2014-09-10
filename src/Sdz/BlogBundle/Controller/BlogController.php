12c7daa47592630d6f63ec46ab3c848569cfa9fd
koyomi araragi <nicolas0608@hotmail.fr>
2014-09-10 16:57:33 +0200

    routing error

--- src/Sdz/BlogBundle/Controller/BlogController.php
+++ src/Sdz/BlogBundle/Controller/BlogController.php
@@ -5,16 +5,15 @@ namespace Sdz\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class blogController extends Controller
-	{
-		public function indexAction(){
-			return $this->render('SdzBlogBundle:Blog:index.html.twig', array('nom' => 'hotaru'));
-		}
-		public function voirAction($id){
-			return new Response("affichage de l'article d'id : ".$id.".");
-		}
-		public function voirSlugAction($slug, $annee, $format){
-			return new Response("On pourrait afficher l'article correspondant au slug '".$slug."', créé en ".$annee." et au format ".$format.".");
-		}
+class blogController extends Controller{
+	public function indexAction(){
+		return $this->render('SdzBlogBundle:Blog:index.html.twig', array('nom' => 'hotaru'));
 	}
+	public function voirAction($id){
+		return new Response("affichage de l'article d'id : ".$id.".");
+	}
+	public function voirSlugAction($slug, $annee, $format){
+		return new Response("On pourrait afficher l'article correspondant au slug '".$slug."', créé en ".$annee." et au format ".$format.".");
+	}
+}
 
